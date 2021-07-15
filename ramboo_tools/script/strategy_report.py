7282ef0ad7c55df121b0a30d55826f43a4feb86c
yuanhui.r9 <yuanhui.r9@bytedance.com>
2021-07-15 22:23:44 +0800

    [Add]添加strategy_report，输出策略报告（分类策略）

--- ramboo_tools/script/strategy_report.py
+++ ramboo_tools/script/strategy_report.py
@@ -11,7 +11,7 @@ class StrategyReportProcessor(StreamProcessor):
     策略指标报告
     """
 
-    rows_result_default = ['ERROR']
+    fixed_column_width = 2
 
     def __init__(self):
         super().__init__()
@@ -23,9 +23,14 @@ class StrategyReportProcessor(StreamProcessor):
         parser.add_argument('-f2', '--field_num_2', default=2, type=int, help='startegy res field num')
         parser.add_argument('-lt', '--label_target', type=str, help='convert label to 0/1(match target or not)')
         parser.add_argument(
-            '-st', '--strategy_target', type=str, help='convert res to 0/1(match target or not). <value>: res==value[default] / len: len(res)'
+            '-pt', '--predict_target', type=str, help='convert res to 0/1(match target or not). <value>: res==value[default] / len: len(res)'
         )
+        # parser.add_argument('-t', '--target', type=str, help='common target (as label_target & predict_target)')
+        parser.add_argument('-ls', '--labels_skip', action='append', type=str, default=[], help='skip label values (append)')
+        parser.add_argument('-ps', '--predicts_skip', action='append', type=str, default=['-', 'error'], help='skip predict values (append)')
+        parser.add_argument('-s', '--skip', type=str, default=[], help='common skip (as labels_skip & predicts_skip)')
         parser.add_argument('-l', '--label_names', action='append', type=str, help='label names(append)')
+        parser.add_argument('-d', '--need_detail', action='store_true', help='whether to print detail info')
 
     def _before_process(self, *args, **kwargs):
         self.y_actual_list = []
@@ -34,32 +39,41 @@ class StrategyReportProcessor(StreamProcessor):
     def rows_process(self, rows, *objects, **kwargs):
         field_num = self.cmd_args.get('field_num', 1)
         label = rows[field_num - 1]
-        startegy_res_field = self.cmd_args.get('field_num_2', 2)
-        startegy_res = rows[startegy_res_field - 1]
+        predict_field_num = self.cmd_args.get('field_num_2', 2)
+        predict = rows[predict_field_num - 1]
         label_target = self.cmd_args.get('label_target')
-        strategy_target = self.cmd_args.get('strategy_target')
+        predict_target = self.cmd_args.get('predict_target')
+        skip = self.cmd_args.get('skip', [])
+        predicts_skip = self.cmd_args.get('predicts_skip', ['-', 'error'])
+        labels_skip = self.cmd_args.get('labels_skip', [])
+        predicts_skip += skip
+        labels_skip += skip
+        need_detail = self.cmd_args.get('need_detail', False)
+        if label.lower() in labels_skip:
+            return '-', '-'
         if label_target is not None:
             label = label == label_target
         label = int(label)
-        if startegy_res.lower() in ['-', 'error']:
-            return None
-        if strategy_target == 'len':
-            startegy_res = bool(len(startegy_res))
-        elif strategy_target is None:
+        if predict.lower() in predicts_skip:
+            return label, '-'
+        if predict_target == 'len':
+            predict = bool(len(predict))
+        elif predict_target is None:
             pass
         else:
-            startegy_res = strategy_target == startegy_res
-        startegy_res = int(startegy_res)
-        if self.line_count <= 10:
-            logging.debug(f'rows:{rows} label[{label}] startegy_res[{startegy_res}]')
+            predict = predict_target == predict
+        predict = int(predict)
         self.y_actual_list.append(label)
-        self.y_predict_list.append(startegy_res)
+        self.y_predict_list.append(predict)
+        if need_detail:
+            return label, predict
         return None
 
     def _after_process(self, *args, **kwargs):
         label_names = self.cmd_args.get('label_names')
         assert len(self.y_actual_list) == len(self.y_predict_list), f'label_n[{len(self.y_actual_list)}] res_n[{len(self.y_predict_list)}] unmatch'
         report = metrics.classification_report(self.y_actual_list, self.y_predict_list, target_names=label_names, digits=4)
+        logging.info(report)
         print(report, file=self.output_stream)
 
 
