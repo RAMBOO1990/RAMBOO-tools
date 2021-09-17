1a644529bbdc3c974b73be4d45ee873fa24672bb
yuanhui.r9 <yuanhui.r9@bytedance.com>
2021-09-17 15:58:58 +0800

    [Update]xls2txt

--- ramboo_tools/script/xls2txt.py
+++ ramboo_tools/script/xls2txt.py
@@ -1,34 +1,35 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
-
-"""
-@Time    : 2018-08-02
-@Author  : RAMBOO
-@Desc    : xls转txt 使用pandas库实现
-"""
-
 # 系统库
 from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
 import sys
+import argparse
 
 # 第三方库
 import six
 import pandas
 
-# 内部库
-from ramboo_tools.stream_processor import StreamProcessor
 
+class XlsToTextStreamProcessor(object):
+    def convert(self, text):
+        for str1, str2 in [
+            ('\\', '\\\\'),
+            ('\t', r'\t'),
+            ('\r\n', r'\n'),
+            ('\n', r'\n'),
+        ]:
+            text = str(text).replace(str1, str2)
+        return text
 
-class XlsToTextStreamProcessor(StreamProcessor):
-    # 关闭输入保留
-    keep_input_rows = False
-    # 关闭固定列宽
-    fixed_column_width = -1
-
-    def __init__(self):
-        super().__init__()
+    def process(self, *objects, **kwargs):
+        parser = argparse.ArgumentParser(description='stream processor')
+        parser.add_argument('-i', '--input_excel', required=True, type=argparse.FileType('r'), help='input excel file')
+        parser.add_argument('-output' '--output_stream', default=sys.stdout, type=argparse.FileType('w'), help='output file/stream')
+        parser.add_argument('-sep', '--seperator', default='\t', help=r'i/o rows seperator, \t default')
+        args = parser.parse_args()
+        self.cmd_args = args.__dict__
         self.input_excel = self.cmd_args.get('input_excel', None)
         if not self.input_excel:
             raise ValueError('need -o input_excel')
@@ -38,34 +39,15 @@ class XlsToTextStreamProcessor(StreamProcessor):
             raise ValueError('input exceel file name must be xxx.xls(x)')
         data_frame = pandas.read_excel(self.input_excel.name, header=None)
         data_frame.fillna('', inplace=True)
-        self.input_stream = data_frame.to_records(index=False)
-
-    def convert(self, text):
-        for str1, str2 in [
-            ('\\', '\\\\'),
-            ('\t', r'\t'),
-            ('\r\n', r'\n'),
-            ('\n', r'\n'),
-        ]:
-            text = text.replace(str1, str2)
-        return text
-
-    def rows_process(self, rows=None, *objects, **kwargs):
-        # rows = [row.replace('\n', r'\n').replace('\t', r'\t') if isinstance(row, str) else row for row in rows]
-        rows = list(map(self.convert, rows))
-        # rows = list(map(str, rows))
-        rows = list(map(six.ensure_text, rows))
-        return rows
-
-    def _add_cmd_args(self, parser):
-        import argparse
-
-        parser.add_argument('-i', '--input_excel', required=True, type=argparse.FileType('r'), help='input excel file')
+        data_list = data_frame.to_records(index=False)
+        for rows in data_list:
+            rows = list(map(six.ensure_text, map(self.convert, rows)))
+            print(*rows, sep=self.cmd_args.get('seperator', '\t'), file=self.cmd_args.get('output', sys.stdout))
 
 
 def main():
     processorObj = XlsToTextStreamProcessor()
-    processorObj.stream_process()
+    processorObj.process()
 
 
 if __name__ == '__main__':
