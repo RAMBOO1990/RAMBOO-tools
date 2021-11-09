20ab8066560839bd2cc13643a197468c40afdc27
yuanhui.r9 <yuanhui.r9@bytedance.com>
2021-11-09 17:44:02 +0800

    [Add]pretty_print的-f支持多列（默认逗号分隔）

--- ramboo_tools/script/pretty_print.py
+++ ramboo_tools/script/pretty_print.py
@@ -10,12 +10,25 @@ class PrettyPrintStreamProcessor(StreamProcessor):
     优雅的打印对象（主要对括号进行多行展开）
     """
 
+    def __init__(self):
+        super().__init__()
+        self.field_num_separator = self.cmd_args.get('field_num_separator', ',')
+
     def rows_process(self, rows=None, *objects, **kwargs):
-        field_num = int(self.cmd_args.get('field_num', 1))
-        content = str(rows[field_num - 1])
-        rows[field_num - 1] = pp(content)
+        field_num_list = list(map(int, str(self.cmd_args.get('field_num', '1')).split(',')))
+        for field_num in field_num_list:
+            content = str(rows[field_num - 1])
+            rows[field_num - 1] = pp(content)
         print(*rows, sep=self.separator, file=self.output_stream)
         return None
+
+    def _add_cmd_args(self, parser):
+        """
+        添加命令行参数，子类可覆盖该方法并调用parser.add_argument()添加参数
+        """
+        parser.add_argument(
+            '-s', '--field_num_separator', default=',', type=str, help='separator for -f/--field_num, "," default (i.e: "," for "1,2,3")'
+        )
 
 
 def main():
